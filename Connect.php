a68618f36b260ba3b458bf02e23368a04b8eb0d7
Yashodha Hayumi Caldera <yash893haumi@gmail.com>
2025-09-28 22:25:05 +0530

    Add files via upload

---
+++ Connect.php
@@ -0,0 +1,9 @@
+<?php
+    $name = $_POST['name'];
+    $companyname = $_POST['companyname'];
+    $contactnumber = $_POST['contactnumber'];
+    $email = $_POST['email'];   
+    $message = $_POST['message'];   
+
+
+?>
